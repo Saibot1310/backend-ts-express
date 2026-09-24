b758fe93a1d80b8674b537b999fd8fd2a56fad16
Tobias Vega <tobivega1310@gmail.com>
2026-09-24 08:44:58 -0300

    feat: add data users

---
+++ src/data/users.data.ts
@@ -0,0 +1,22 @@
+import type { User } from "../models/user.model.js";
+
+export const users: User[] = [
+  {
+    id: 1,
+    name: "Juan Pérez",
+    email: "juan@example.com",
+    age: 25,
+  },
+  {
+    id: 2,
+    name: "María López",
+    email: "maria@example.com",
+    age: 30,
+  },
+  {
+    id: 3,
+    name: "Carlos Gómez",
+    email: "carlos@example.com",
+    age: 22,
+  },
+];
